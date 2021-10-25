cce8874cfa6f955c6dc7eb568954ca7284c53642
Laysa Santiago <55203249+Laysanha@users.noreply.github.com>
2021-10-25 19:24:05 -0300

    POO lampada

---
+++ Lampada.java
@@ -0,0 +1,20 @@
+//package orientadoObjectoexemplo2;
+//
+//public class Lampada {
+//	
+//	public int potencia;
+//	public boolean acesa;
+//
+//	public void acender() {
+//		if (acesa==true) {
+//			System.out.println("Já acesa");
+//		}
+//		acesa = true;
+//	}
+//	public void apagar() {
+//		if (acesa==false) {
+//			System.out.println("Apagada");
+//		}
+//		acesa = false;
+//	}
+//}
---
+++ TesteLampada.java
@@ -0,0 +1,18 @@
+//package orientadoObjectoexemplo2;
+//
+//public class TesteLampada {
+//	public static void main(String[] args) {
+//		Lampada l = new Lampada();
+//		l.potencia = 60;
+//		l.acesa = false;
+//		
+//		
+//		System.out.println("Situação: " + l.acesa);
+//		
+//		l.apagar();
+//		System.out.println("Situação 1: " + l.acesa);
+//		
+//		l.apagar();
+//		System.out.println("Situação 1: " + l.acesa);
+//	}
+//}
